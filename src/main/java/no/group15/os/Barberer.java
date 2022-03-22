268107db4f7616270761e7227b7f9508ce5dd145
Kenneth <Kennetej@stud.ntnu.no>
2022-03-22 18:28:49 +0100

    Started on the barberer class and made Exceptions needed for this class.

--- src/main/java/no/group15/os/Barberer.java
+++ src/main/java/no/group15/os/Barberer.java
@@ -1,15 +1,52 @@
 package no.group15.os;
 
+import no.group15.os.exceptions.CouldNotAddCustomerException;
+import no.group15.os.exceptions.CouldNotGetCustomerException;
+import no.group15.os.exceptions.CouldNotRemoveCustomerException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * @author Steinar Hjelle Midthus
+ * @author Kenneth Johansen Misund.
  * @version 0.1
  */
 public class Barberer {
 
+    List<Customer> customerList;
+    private String salonName;
+
+
     /**
      * Makes an instance of the Barberer class.
      */
-    public Barberer() {
+    public Barberer(String salon) {
+        this.customerList = new ArrayList<>(5);
+        checkString(salon, "salon name");
+    }
+
+    /**
+     * Adds a customer to a list that could hold max 5 elements.
+     * @param customer to be added.
+     * @throws CouldNotAddCustomerException gets thrown if the customer could not be added.
+     */
+    public void addCustomer(Customer customer) throws CouldNotAddCustomerException {
+    }
+
+    /**
+     * Remove a customer from the list that holds max 5 elements.
+     * @param customer to be removed form list.
+     * @throws CouldNotRemoveCustomerException gets thrown if the customer could not be removed.
+     */
+    public void removeCustomer(Customer customer) throws CouldNotRemoveCustomerException {
+
+    }
+
+    /**
+     * Gets the next customer from the list when called.
+     * @throws CouldNotGetCustomerException gets thrown when you couldn't get the next customer.
+     */
+    public void getNextCustomer() throws CouldNotGetCustomerException {
 
     }
 
@@ -20,6 +57,7 @@ public class Barberer {
      * @param errorPrefix   the error the exception should have if the string is invalid.
      * @throws IllegalArgumentException gets thrown if the string to check is empty or null.
      */
+
     private void checkString(String stringToCheck, String errorPrefix) {
         checkIfObjectIsNull(stringToCheck, errorPrefix);
         if (stringToCheck.isEmpty()) {
---
+++ src/main/java/no/group15/os/exceptions/CouldNotGetCustomerException.java
@@ -0,0 +1,19 @@
+package no.group15.os.exceptions;
+
+import java.io.Serializable;
+
+/**
+ * CouldNotGetCustomerException represents an exception that gets thrown when couldn't get the customer.
+ * @author Kenneth Johansen Misund
+ * @version 0.1
+ */
+public class CouldNotGetCustomerException extends Exception implements Serializable {
+
+    /**
+     * Makes an instance of the CouldNotGetCustomerException class.
+     * @param message the error message to be displayed.
+     */
+    public CouldNotGetCustomerException(String message) {
+        super(message);
+    }
+}
